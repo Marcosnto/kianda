5fad95e4c5518576309579265f11b56fbf08eee5
Marcos Neto <marcos_nto@hotmail.com>
2023-04-09 22:44:12 -0300

    feat: create admin page

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -1,10 +1,43 @@
-import HeaderCMS from "@/components/HeaderCMS/HeaderCMS";
-import { Box } from "@chakra-ui/react";
+import HeaderCMS from "@/components/CMS/HeaderCMS";
+import Menu from "@/components/CMS/Menu";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 export default function Admin() {
   return (
-    <Box>
-      <HeaderCMS />
-    </Box>
+    <Grid
+      h="100vh"
+      templateAreas={[
+        `
+        "header header"
+        "content content"
+        "content content"
+        "footer footer"
+      `,
+        `
+        "header header"
+        "sidenav content"
+        "sidenav content"
+        "footer footer"
+      `,
+      ]}
+      gridTemplateRows={"4.3rem auto auto 2rem"}
+      gridTemplateColumns={"1fr 2fr"}
+    >
+      <GridItem area={"header"}>
+        <HeaderCMS />
+      </GridItem>
+
+      <GridItem display={["none", "block"]} area={"sidenav"}>
+        <Menu />
+      </GridItem>
+
+      <GridItem area={"content"} background="gray.100">
+        <div>content</div>
+      </GridItem>
+
+      <GridItem area={"footer"} background="gray.100">
+        <div>footer</div>
+      </GridItem>
+    </Grid>
   );
 }

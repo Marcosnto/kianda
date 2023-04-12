6d5e9cc3d28897e514b944d6e1105eb3d4f52429
Marcos Neto <marcos_nto@hotmail.com>
2023-04-12 15:07:03 -0300

    feat: create users mock

---
+++ src/data/usersMock.ts
@@ -0,0 +1,20 @@
+const users = [
+  {
+    name: "Marcos Neto Santos",
+    status: "Cancelado",
+  },
+  {
+    name: "Luiz Filipe Santos",
+    status: "Pausado",
+  },
+  {
+    name: "Pedro Sampaio",
+    status: "ativo",
+  },
+  {
+    name: "Camila Tigre",
+    status: "Ativo",
+  },
+];
+
+export default users;

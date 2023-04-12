e1f5527a884488b85545e5062b5d4f2767809eea
Marcos Neto <marcos_nto@hotmail.com>
2023-04-12 15:05:45 -0300

    feat: create CMS Menu for desktop

--- src/components/CMS/Menu/index.tsx
+++ src/components/CMS/Menu/index.tsx
@@ -1,14 +1,41 @@
-import { Flex } from "@chakra-ui/react";
+import { Divider, Flex, Heading, MenuDivider } from "@chakra-ui/react";
 import MenuOptions from "../MenuOptions";
 import { admin } from "@/data/menuOptionsPermitions";
+import { ReactElement } from "react";
 
-export default function Menu() {
+export default function Menu({ setCurrentComponent }: any) {
   return (
     <Flex flexDir="column" pt="4">
-      {admin.options.map((option) => (
+      {admin.users ? (
+        <Heading as="h3" size="sm" noOfLines={1} ml="3" mt="8">
+          Pacientes
+        </Heading>
+      ) : null}
+
+      {admin.users?.map((option) => (
         <MenuOptions
           key={option.key}
           icon={option.icon}
+          render={option.render}
+          setCurrentComponent={setCurrentComponent}
+          displayName={option.displayName}
+        />
+      ))}
+
+      {admin.users ? (
+        <>
+          <Divider mt="8" />
+          <Heading as="h3" size="sm" noOfLines={1} ml="3" mt="8">
+            Blog
+          </Heading>
+        </>
+      ) : null}
+      {admin.blog.map((option) => (
+        <MenuOptions
+          key={option.key}
+          icon={option.icon}
+          render={option.render}
+          setCurrentComponent={setCurrentComponent}
           displayName={option.displayName}
         />
       ))}

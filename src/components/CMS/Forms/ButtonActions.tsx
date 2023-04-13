e6d9d33cf0c2a44f6b86748b238f4bf0e8933901
Marcos Neto <marcos_nto@hotmail.com>
2023-04-13 09:49:00 -0300

    feat: create action buttons group component

---
+++ src/components/CMS/Forms/ButtonActions.tsx
@@ -0,0 +1,41 @@
+import { IconButton, IconButtonProps, Stack } from "@chakra-ui/react";
+import { MouseEventHandler } from "react";
+import {
+  IoArchiveOutline,
+  IoCreateOutline,
+  IoTrashOutline,
+} from "react-icons/io5";
+
+export default function ButtonActions() {
+  function getIconButton(
+    ariaLabel: string,
+    icon: React.ReactElement,
+    onClick: () => void
+  ) {
+    return (
+      <IconButton
+        w="8"
+        h="8"
+        fontSize="18"
+        variant="outline"
+        aria-label={ariaLabel}
+        icon={icon}
+        onClick={onClick}
+      />
+    );
+  }
+
+  return (
+    <Stack spacing={2} direction="row">
+      {getIconButton("Criar Artigo", <IoCreateOutline />, () =>
+        console.log("icon1")
+      )}
+      {getIconButton("Remover Artigo", <IoTrashOutline />, () =>
+        console.log("icon2")
+      )}
+      {getIconButton("Arquivar artigo", <IoArchiveOutline />, () =>
+        console.log("icon3")
+      )}
+    </Stack>
+  );
+}

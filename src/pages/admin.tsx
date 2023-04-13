7099bcc80128b7ab693045d28ec6f5b8917666d2
Marcos Neto <marcos_nto@hotmail.com>
2023-04-13 09:06:41 -0300

    fix: adjust on Menu grid

--- src/pages/admin.tsx
+++ src/pages/admin.tsx
@@ -2,7 +2,7 @@ import Content from "@/components/CMS/Content";
 import HeaderCMS from "@/components/CMS/HeaderCMS";
 import Menu from "@/components/CMS/Menu";
 import { Grid, GridItem } from "@chakra-ui/react";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Admin() {
   const [currentComponent, setCurrentComponent] = useState(<div>Teste</div>);
@@ -31,7 +31,14 @@ export default function Admin() {
       `,
       ]}
       gridTemplateRows={"4.3rem auto auto 2rem"}
-      gridTemplateColumns={["none", "none", "1.5fr 3fr", "1fr 3fr"]}
+      gridTemplateColumns={[
+        "none",
+        "none",
+        "1.3fr 3fr",
+        "1fr 3fr",
+        "0.8fr 3fr",
+        "0.6fr 3fr",
+      ]}
     >
       <GridItem area={"header"} background={"yellow.800"} color="#FFF">
         <HeaderCMS setCurrentComponent={setCurrentComponent} />

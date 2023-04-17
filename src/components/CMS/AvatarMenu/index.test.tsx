4683f492dcf696511efe9b1d05a64665fff47cef
Marcos Neto <marcos_nto@hotmail.com>
2023-04-17 11:07:01 -0300

    fix: edit example test

---
+++ src/components/CMS/AvatarMenu/index.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from "@testing-library/react";
+import AvatarMenu from ".";
+
+describe("Avatar Menu", () => {
+  it("should show Avatar Menu on screen", async () => {
+    expect(true).toBe(true);
+  });
+});

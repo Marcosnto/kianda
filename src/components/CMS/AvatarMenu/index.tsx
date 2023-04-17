3f1375d480f69f7a52be0096ebddebb58280cee1
Marcos Neto <marcos_nto@hotmail.com>
2023-04-17 11:07:42 -0300

    fix: redirect 'Sair' AvatarMenu option to login page

--- src/components/CMS/AvatarMenu/index.tsx
+++ src/components/CMS/AvatarMenu/index.tsx
@@ -1,10 +1,19 @@
 import { AddIcon, ArrowForwardIcon, LockIcon } from "@chakra-ui/icons";
 import { Avatar, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import { useRouter } from "next/router";
+
+type AvatarMenuProps = {
+  icon: React.ReactElement;
+  content: string;
+  onClick?: () => void;
+};
 
 export default function AvatarMenu() {
-  function MenuItemComponent(icon: React.ReactElement, content: string) {
+  const router = useRouter();
+
+  function MenuItemComponent({ icon, content, onClick }: AvatarMenuProps) {
     return (
-      <MenuItem _hover={{ bg: "orange.50" }} icon={icon}>
+      <MenuItem _hover={{ bg: "orange.50" }} icon={icon} onClick={onClick}>
         {content}
       </MenuItem>
     );
@@ -17,14 +26,24 @@ export default function AvatarMenu() {
           as={Avatar}
           aria-label="Opções do usuário"
           icon={
-            <Avatar name="Dan Abrahmov" src="https://bit.ly/sage-adebayo" />
+            <Avatar
+              name="Adelmo Filho"
+              src="https://pbs.twimg.com/profile_images/1610774407852560385/i0ONtDeh_400x400.jpg"
+            />
           }
           variant="outline"
         />
         <MenuList sx={{ color: "black" }}>
-          {MenuItemComponent(<AddIcon />, "Ver Perfil")}
-          {MenuItemComponent(<LockIcon />, "Redefinir Senha")}
-          {MenuItemComponent(<ArrowForwardIcon />, "Sair")}
+          {MenuItemComponent({ icon: <AddIcon />, content: "Ver Perfil" })}
+          {MenuItemComponent({
+            icon: <LockIcon />,
+            content: "Redefinir Senha",
+          })}
+          {MenuItemComponent({
+            icon: <ArrowForwardIcon />,
+            content: "Sair",
+            onClick: () => router.push("/login"),
+          })}
         </MenuList>
       </Menu>
     </span>

09823f28f2c5361c32fd118f2241d5768282d8a7
Marcos Neto <marcos_nto@hotmail.com>
2023-04-30 23:51:28 -0300

    feat: create login handler for test

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,27 +1,48 @@
-// type LoginProps = {
-//   email: string;
-//   password: string;
-// };
+import type { NextApiRequest, NextApiResponse } from "next";
+import cookie from "cookie";
 
-// async function fetchLogin(data: LoginProps) {
-//   const options = {
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     method: "POST",
-//     body: JSON.stringify({ username: data.email, password: data.password }),
-//   };
+type Data = {
+  email: string;
+};
 
-//   const apiRes = await fetch(
-//     "http://tns-back.local/wp-json/jwt-auth/v1/token",
-//     options
-//   );
+async function loginHandler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== "POST") {
+    res.status(405).end();
+    return;
+  }
 
-//   if (!apiRes.ok) {
-//     throw new Error("Something go wrong on login");
-//   }
+  const { email, password } = req.body;
 
-//   return apiRes.json();
-// }
+  try {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({ username: email, password: password }),
+    };
 
-// export default fetchLogin;
+    const apiRes = await fetch(process.env.NEXT_PUBLIC_JWT_URL || "", options);
+
+    if (!apiRes.ok) {
+      res.status(401).end();
+    }
+
+    apiRes.json().then((response) => {
+      res
+        .status(200)
+        .setHeader(
+          "Set-cookie",
+          cookie.serialize("token", response.token, {
+            path: "/",
+            httpOnly: true,
+          })
+        )
+        .json({
+          email: email,
+        });
+    });
+  } catch (error) {
+    console.log("Erro: ", error);
+    res.status(401).end();
+  }
+}
+
+export default loginHandler;
